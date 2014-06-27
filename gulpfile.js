efdb1093c1a1ba5caa945a3cbc58f6f7d8554787
MichaelHsu <evenchange4@gmail.com>
2014-06-27 17:25:05 +0800

    <script src=bower_components/angular/angular.min.js></script>

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
-  connect = require('gulp-connect');
+  connect = require('gulp-connect'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify');
 
 gulp.task('connect', function() {
   connect.server({
@@ -25,4 +27,22 @@ gulp.task('watch', function () {
   // gulp.watch(['./js/michaelhsu.tw/*.js']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('scripts', function() {
+  gulp.src(['bower_components/*/*.js','js/*.js', 'js/*/*.js'])
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('build/'))
+});
+
+// gulp.task('scripts-bower', function() {
+//   gulp.src('bower_components/*/*.js')
+//     .pipe(concat('all2.js'))
+//     .pipe(gulp.dest('build/'))
+// });
+
+gulp.task('compress', function() {
+  gulp.src('build/all.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build'))
+});
+
+gulp.task('default', ['scripts', 'connect', 'watch']);
